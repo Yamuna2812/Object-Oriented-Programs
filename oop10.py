5d56c6c817cd94b83d67e8833a82ef0c723a8868
Yamuna <hyamuna2807@gmail.com>
2026-05-05 21:30:56 +0530

    Add files via upload

---
+++ oop10.py
@@ -0,0 +1,18 @@
+class ATM:
+    def __init__(self, balance):
+        self.__balance = balance  # Private attribute
+
+    def deposit(self, amount):
+        self.__balance += amount
+        print(f"Deposited {amount}. New balance: {self.__balance}")
+
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+            print(f"Withdrew {amount}. New balance: {self.__balance}")
+        else:
+            print("Insufficient balance")
+
+atm = ATM(1000)
+atm.deposit(500)
+atm.withdraw(300)
