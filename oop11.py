98cc5de3500bb02ce8c83f1cd0d3d70b594c3022
Yamuna <hyamuna2807@gmail.com>
2026-05-08 17:45:37 +0530

    Add files via upload

--- oop11.py
+++ oop11.py
@@ -1,15 +1,18 @@
-class User:
-    def __init__(self, username, password):
-        self.username = username
-        self.__password = password  # Private attribute
+class ATM:
+    def __init__(self, balance):
+        self.__balance = balance  # Private attribute
 
-    def get_username(self):
-        return self.username
+    def deposit(self, amount):
+        self.__balance += amount
+        print(f"Deposited {amount}. New balance: {self.__balance}")
 
-    def check_password(self, password):
-        return password == self.__password
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+            print(f"Withdrew {amount}. New balance: {self.__balance}")
+        else:
+            print("Insufficient balance")
 
-user = User("dev_karnataka", "pass1234")
-print(user.get_username())  # Access allowed
-print(user.check_password("wrong_pass"))  # Returns False
-print(user.check_password("pass1234"))  # Returns True
+atm = ATM(1000)
+atm.deposit(500)
+atm.withdraw(1000)
