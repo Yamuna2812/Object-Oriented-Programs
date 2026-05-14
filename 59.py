a7ea593870d4cb067e36252c2867f9ef485676a7
Yamuna <hyamuna2807@gmail.com>
2026-05-14 20:36:21 +0530

    Create 59.py

---
+++ 59.py
@@ -0,0 +1,10 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+r = Rectangle(5, 3)
+print("Area:", r.area())
